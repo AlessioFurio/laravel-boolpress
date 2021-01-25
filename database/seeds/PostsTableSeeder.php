d87f2189c1588abeea4f5fad48e107353cf2f917
Furio Alessio <alefurio@hotmail.it>
2021-01-25 15:35:04 +0100

    generazione dati db con faker

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Post;
+use Faker\Generator as Faker;
 
 class PostsTableSeeder extends Seeder
 {
@@ -10,31 +11,39 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $posts = [
-            [
-                'name' => 'Alessio',
-                'lastname' => 'Furio',
-                'date_of_birth' => '1993-01-30'
-            ],
-            [
-                'name' => 'Flavio',
-                'lastname' => 'Furio',
-                'date_of_birth' => '2003-01-15'
-            ],
-            [
-                'name' => 'Cecilia',
-                'lastname' => 'Brillante',
-                'date_of_birth' => '1965-10-08'
-            ],
-        ];
+        // $posts = [
+        //     [
+        //         'name' => 'Alessio',
+        //         'lastname' => 'Furio',
+        //         'date_of_birth' => '1993-01-30'
+        //     ],
+        //     [
+        //         'name' => 'Flavio',
+        //         'lastname' => 'Furio',
+        //         'date_of_birth' => '2003-01-15'
+        //     ],
+        //     [
+        //         'name' => 'Cecilia',
+        //         'lastname' => 'Brillante',
+        //         'date_of_birth' => '1965-10-08'
+        //     ],
+        // ];
 
-        foreach ($posts as $item) {
+        // foreach ($posts as $item) {
+        //     $new_post = new Post();
+        //     $new_post->name = $item['name'];
+        //     $new_post->lastname = $item['lastname'];
+        //     $new_post->date_of_birth = $item['date_of_birth'];
+        //     $new_post->save();
+        // }
+
+        for ($i=0; $i < 10 ; $i++) {
             $new_post = new Post();
-            $new_post->name = $item['name'];
-            $new_post->lastname = $item['lastname'];
-            $new_post->date_of_birth = $item['date_of_birth'];
+            $new_post->name = $faker->FirstName();
+            $new_post->lastname = $faker->lastName();
+            $new_post->date_of_birth = $faker->date();
             $new_post->save();
         }
     }
