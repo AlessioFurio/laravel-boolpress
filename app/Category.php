39e2f381fb0449309d666d14c0fabaeb2bdd8a0e
Furio Alessio <alefurio@hotmail.it>
2021-01-27 23:57:45 +0100

    creata relazione tra category e post

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function posts(){  // posts al plurale perche' una category puo' avere piu' posts collegati ad essa
+        return $this->hasMany('App\Post'); //Category ha tanti post associati quindi avra' la relazione hasMany che la mappera' con App\Post
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'content', 'slug'];
+
+    public function category(){  // tabella secondaria, dipendente da posts
+        return $this->belongsTo('App\Category'); // sapendo che e' la tabella secondaria ci andra' x forza ->belongsTo
+    }
 }
