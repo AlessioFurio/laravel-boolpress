b84a91d67475bb001ac534a0b03ae36c64c48dac
Furio Alessio <alefurio@hotmail.it>
2021-02-01 16:27:57 +0100

    funzione old() nel form della route admin.posts.create

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -25,12 +25,12 @@
                 @csrf
                 <div class="form-group">
                     <label>Titolo</label>
-                    <input type="text" name="title" value="{{ old('title') }}" class="form-control" placeholder="Inserisci il titolo" maxlength="255" required>
+                    <input type="text" name="title" value="{{ old('title') }}" class="form-control" placeholder="Inserisci il titolo" required> {{--la funzione old() mi permette di "salvare i dati dell' utente e mostrarglieli anche se la creazione del post fallisce in modo da non dover ricompilare i campi"--}}
                 </div>
 
                 <div class="form-group">
                     <label>Contenuto</label>
-                    <textarea name="content" class="form-control" rows="10" placeholder="Inizia a scrivere qualcosa..." required></textarea>
+                    <textarea name="content" class="form-control" rows="10" placeholder="Inizia a scrivere qualcosa..." required>{{ old('content') }}</textarea>
                 </div>
 
                 <div class="form-group">
@@ -38,7 +38,7 @@
                     <select class="form-control" name="category_id"> {{-- per inviare i dati con il form dobbiamo associare un name che sara' category_id perche' la categoria che seleziono nella select, andra' ad impostarsi nella colonna category_id della tabella posts --}}
                         <option value="">-- seleziona categoria --</option>
                         @foreach ($categories as $category)
-                            <option value="{{ $category->id }}"> {{-- come value passo $category->id perche' deve collegarsi a category_id e definirlo --}}
+                            <option value="{{$category->id}}" {{old('category_id') == $category->id ? 'selected=selected' : ''}}> {{-- come value passo $category->id perche' deve collegarsi a category_id e definirlo --}}
                                 {{ $category->name }} {{-- popolo la option con le varie categorie prese dal model Category --}}
                             </option>
                         @endforeach
@@ -48,7 +48,7 @@
                 <div class="form-group">
                     <p>Tags</p>
                     @foreach ($tags as $tag)
-                        <input type="checkbox" name="tags_selected[]" value="{{$tag->id}}">
+                        <input type="checkbox" name="tags_selected[]" value="{{$tag->id}}" {{ in_array($tag->id, old('tags_selected', [])) ? 'checked=checked' : ''}}> {{--verifico con in_array che $tag->id sia presente nell'array old('tags_selected'), se non e' presente, la funzione old utilizzera' il valore di fallback [] array vuoto in modo da non restituire errore se l'utente non ha selezionato nessun tag--}}
                         <label for="">{{$tag->name}}</label>
                     @endforeach
                 </div>
